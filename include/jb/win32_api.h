9b9f2f77b86bf913afb38b6d2ba32c07257c01a1
Alexey Pavlyutkin <alexey.pavlyutkin@gmail.com>
2020-01-28 22:51:45 +0300

    developing storage_file class: sharing from home

--- include/jb/win32_api.h
+++ include/jb/win32_api.h
@@ -12,6 +12,7 @@
 #include <cstdio>
 #include <exception>
 #include <thread>
+#include <functional>
 #include <windows.h>
 
 
@@ -23,34 +24,37 @@ namespace jb
         {
             struct close_handle
             {
-                void operator()( HANDLE handle )
+                void operator()( HANDLE handle ) noexcept
                 {
                     if ( INVALID_HANDLE_VALUE != handle ) ::CloseHandle( handle );
                 }
             };
             using safe_handle = std::unique_ptr < void, close_handle >;
 
-            alignas( std::hardware_destructive_interference_size ) std::atomic_size_t map_count_ = 0;
-            alignas( std::hardware_destructive_interference_size ) std::atomic_bool mapping_exists_ = false;
+            struct unmap_area
+            {
+                api * api_;
+                void operator()( void* p ) noexcept { api_->unmap( p ); }
+            };
+            using safe_mapped_area = std::unique_ptr< void, unmap_area >;
+
+            using shared_lock = int;
+
             std::filesystem::path path_;
-            safe_handle exclusive_lock_;
+            bool newly_created_ = false;
+            safe_handle interprocess_lock_;
             safe_handle file_;
             safe_handle mapping_;
 
-        public:
-            
-            api() = delete;
-            api( api&& ) = delete;
-
-            explicit api( std::filesystem::path&& path ) : path_( std::move( path ) )
+            safe_handle get_interprocess_lock()
             {
                 // make-up unique mutex name of stack
                 std::array< char, 20 > mutex_name;
                 snprintf( mutex_name.data(), 20, "jb_%zx", std::filesystem::hash_value( path_ ) );
 
                 // try to create named mutex
-                exclusive_lock_ = safe_handle( ::CreateMutexA( NULL, FALSE, mutex_name.data() ), close_handle() );
-                if ( !exclusive_lock_ )
+                safe_handle lock( ::CreateMutexA( NULL, FALSE, mutex_name.data() ), close_handle() );
+                if ( !lock )
                 {
                     // notify that unknown error occuder
                     throw details::runtime_error( RetCode::UnknownError, "Unable to create named mutex" );
@@ -61,12 +65,64 @@ namespace jb
                     throw details::runtime_error( RetCode::AlreadyInUse, "The file is already in use" );
                 }
 
-                // try to open or create the file
-                file_ = safe_handle( ::CreateFileW( path_.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL ), close_handle() );
+                return lock;
+            }
+
+            safe_handle open_file()
+            {
+                safe_handle file( ::CreateFileW( path_.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL ), close_handle() );
                 if ( INVALID_HANDLE_VALUE == file_.get() )
                 {
                     throw details::runtime_error( RetCode::CannotOpenFile, "Unable to open/create storage file" );
                 }
+
+                if ( ERROR_ALREADY_EXISTS != ::GetLastError() )
+                {
+                    newly_created_ = true;
+
+                    LARGE_INTEGER inc;
+                    inc.QuadPart = static_cast<LONGLONG>( page_size() );
+                    if ( !::SetFilePointerEx( file.get(), inc, NULL, FILE_END ) || !::SetEndOfFile( file.get() ) )
+                    {
+                        throw details::runtime_error( RetCode::IoError, "Unable to resize file" );
+                    }
+                }
+
+                return file;
+            }
+
+            safe_handle create_mapping()
+            {
+                assert( INVALID_HANDLE_VALUE != file_.get() );
+
+                // create mapping object
+                safe_handle mapping{ ::CreateFileMappingW( file_.get(), NULL, PAGE_READWRITE, 0, 0, NULL ), close_handle() };
+                if ( !mapping )
+                {
+                    throw details::runtime_error( RetCode::IoError, "Unable to create mapping object" );
+                }
+
+                return mapping;
+            }
+
+        public:
+            
+            api() = delete;
+            api( api&& ) = delete;
+
+            explicit api( std::filesystem::path&& path ) try
+                : path_( std::move( path ) )
+                , interprocess_lock_( std::move( get_interprocess_lock() ) )
+                , file_( std::move( open_file() ) )
+                , mapping_( std::move( create_mapping() ) )
+            {
+            }
+            catch ( const std::exception & e )
+            {
+                throw;
+            }
+            catch ( ... )
+            {
             }
 
             static size_t page_size() noexcept
@@ -78,11 +134,6 @@ namespace jb
 
             size_t size() const
             {
-                if ( INVALID_HANDLE_VALUE == file_.get() )
-                {
-                    throw std::logic_error( "An attempt to get size of non-existing file" );
-                }
-
                 LARGE_INTEGER sz;
                 if ( !::GetFileSizeEx( file_.get(), &sz ) )
                 {
@@ -96,11 +147,6 @@ namespace jb
             {
                 assert( INVALID_HANDLE_VALUE != file_.get() );
 
-                if ( mapping_exists_.load( std::memory_order_acquire ) )
-                {
-                    throw std::logic_error( "grow() called for unmapped file" );
-                }
-
                 LARGE_INTEGER inc;
                 inc.QuadPart = static_cast< LONGLONG >( page_size() );
                 if ( !::SetFilePointerEx( file_.get(), inc, NULL, FILE_END ) || !::SetEndOfFile( file_.get() ) )
@@ -109,9 +155,9 @@ namespace jb
                 }
             }
 
-            void * map( size_t offset )
+            safe_mapped_area map( size_t offset )
             {
-                assert( INVALID_HANDLE_VALUE != file_.get() );
+                assert( mapping_.get() );
 
                 // check offset
                 if ( offset % page_size() )
@@ -123,33 +169,23 @@ namespace jb
                     throw std::logic_error( "Requested mapping offset exceeds file size" );
                 }
 
-                if ( map_count_.fetch_add( 1, std::memory_order_acq_rel ) )
-                {
-                    // create mapping object
-                    safe_handle mapping( ::CreateFileMappingW( file_.get(), NULL , PAGE_READWRITE , 0, 0, NULL ), close_handle() );
-                    if ( INVALID_HANDLE_VALUE == mapping.get() )
-                    {
-                        throw details::runtime_error( RetCode::IoError, "Unable to create mapping object" );
-                    }
+                // map a page from given offset, assign the pointer to safe variable, and return
+                safe_mapped_area mapped_page{
+                    ::MapViewOfFile( mapping_.get(), FILE_MAP_WRITE, offset / ( 1ULL << 32 ), offset % ( 1ULL << 32 ), page_size() ),
+                    unmap_area{ this } 
+                };
 
-                    // preserve mapping object and let other thread know that mapping object exists
-                    mapping_ = std::move( mapping );
-                    mapping_exists_.store( true, std::memory_order_release );
+                if ( !mapped_page )
+                {
+                    throw details::runtime_error( RetCode::IoError, "Unable to map file into memory" );
                 }
 
-                // wait for mapping object 
-                while ( !mapping_exists_.load( std::memory_order_acquire ) ) std::this_thread::yield();
-
-                //
-                static constexpr auto dword_delimiter = ( 1ULL << 32 );
+                return mapped_page;
             }
 
-            void unmap( void * base_address )
+            void unmap( void * base_address ) noexcept
             {
-                if ( !::UnmapViewOfFile( base_address ) )
-                {
-                    throw details::runtime_error( RetCode::IoError, "An error occured upon unmapping" );
-                }
+                ::UnmapViewOfFile( base_address );
             }
         };
     }
--- test/storage_file.cpp
+++ test/storage_file.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <jb/storage.h>
 #include <jb/storage_file.h>
+#include <future>
+
 
 namespace jb
 {
@@ -23,6 +25,7 @@ namespace jb
             }
         };
 
+
         TEST_F( storage_file_test, exclusive_access )
         {
             {
@@ -37,13 +40,44 @@ namespace jb
             EXPECT_NO_THROW( f1 = std::make_unique< storage_file >( "./foo.jb" ) );
         }
 
+        TEST_F( storage_file_test, exclusive_access_multithreading )
+        {
+            std::atomic_bool run = false;
+            auto thread_number = std::thread::hardware_concurrency() - 1;
+
+            if ( thread_number > 1 )
+            {
+                std::vector< std::future< std::unique_ptr< storage_file > > > futures( thread_number );
+
+                for ( auto& f : futures )
+                {
+                    f = std::async( std::launch::async,
+                        [&]()
+                        {
+                            while ( !run.load( std::memory_order_acquire ) );
+                            return std::make_unique< storage_file >( "./foo.jb" );
+                        }
+                    );
+                }
+
+                run.store( true, std::memory_order_release );
+
+                size_t failed_thread_number = 0;
+                for ( auto& f : futures )
+                {
+                    try { f.get(); }
+                    catch ( const details::runtime_error & e ) { ++failed_thread_number; }
+                }
+
+                EXPECT_EQ( thread_number - 1, failed_thread_number );
+            }
+        }
 
         TEST_F( storage_file_test, invalid_fpath )
         {
             std::unique_ptr< storage_file > f;
             EXPECT_THROW( f = std::make_unique< storage_file >( "./~!@#$%^&*()_+.jb" ), details::runtime_error );
         }
-
 
         TEST_F( storage_file_test, create_new_auto_grow_size )
         {
@@ -55,7 +89,6 @@ namespace jb
             EXPECT_EQ( storage_file::page_size(), std::filesystem::file_size( "./foo.jb" ) );
         }
 
-
         TEST_F( storage_file_test, open_existing_grow_size )
         {
             {
@@ -65,6 +98,49 @@ namespace jb
                 EXPECT_EQ( 2 * storage_file::page_size(), f->size() );
             }
             EXPECT_EQ( 2 * storage_file::page_size(), std::filesystem::file_size( "./foo.jb" ) );
+        }
+
+        TEST_F( storage_file_test, map_invalid_offset )
+        {
+            std::unique_ptr< storage_file > f;
+            EXPECT_NO_THROW( f = std::make_unique< storage_file >( "./foo.jb" ) );
+            EXPECT_THROW( f->map( 1 ), std::logic_error );
+            EXPECT_THROW( f->map( storage_file::page_size() - 1 ), std::logic_error );
+            EXPECT_THROW( f->map( storage_file::page_size() / 2 ), std::logic_error );
+            EXPECT_THROW( f->map( storage_file::page_size() ), std::logic_error );
+        }
+
+        TEST_F( storage_file_test, map )
+        {
+            {
+                {
+                    std::unique_ptr< storage_file > f;
+                    ASSERT_NO_THROW( f = std::make_unique< storage_file >( "./foo.jb" ) );
+                    ASSERT_NO_THROW( f->grow() );
+                    ASSERT_NO_THROW( f->grow() );
+                }
+
+                std::unique_ptr< storage_file > f;
+                ASSERT_NO_THROW( f = std::make_unique< storage_file >( "./foo.jb" ) );
+
+                for ( auto i = 0; i < 2; ++i )
+                {
+                    decltype( f->map( 0 ) ) mapping_1, mapping_2;
+                    EXPECT_NO_THROW( mapping_1 = f->map( i * storage_file::page_size() ) );
+                    EXPECT_NO_THROW( mapping_2 = f->map( i * storage_file::page_size() ) );
+                    EXPECT_TRUE( mapping_1 );
+                    EXPECT_TRUE( mapping_2 );
+                    //
+                    char* ptr_1 = (char*)mapping_1.get();
+                    char* ptr_2 = (char*)mapping_2.get();
+                    ASSERT_TRUE( ptr_1 && ptr_2 );
+                    //
+                    for ( auto j = 0; j < storage_file::page_size(); ++j, ++ptr_1, ++ptr_2 )
+                    {
+                        EXPECT_EQ( *ptr_1, *ptr_2 );
+                    }
+                }
+            }
         }
     }
 }
